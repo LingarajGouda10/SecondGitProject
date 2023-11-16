dc64fc2510956f24ba8bf8e9f4586920c332b033
Lingaraj Gouda <lingarajg@ivoyant.com>
2023-11-16 10:10:49 +0530

    Init commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+
+class App{
+    protected String batsman="Virat";
+    public void player(){
+        System.out.println("G O A T");
+        class Athlete extends Cricket{
+            private String playerName="Kohli";
+
+            public static void main(String[] args) {
+                Athlete myFav=new Athlete();
+                myFav.player();
+                System.out.println(myFav.batsman + " " + myFav.playerName);
+            }
+        }
+    }
+}
